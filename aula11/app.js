652de70a703b05fb5c23fa44650eadc8de5cee8d
Amauri Carlos <amauricarlosdecarvalho@gmail.com>
2025-06-02 22:49:23 -0300

    Corrige Gambiarra do codigo
    Corrigindo o código do app

--- aula11/app.js
+++ aula11/app.js
@@ -4,6 +4,8 @@ var inputLogradouro = document.getElementById('Logradouro');
 var inputBairro = document.getElementById('Bairro');
 var inputCidade = document.getElementById('Cidade');
 var erro = document.getElementById('erro');
+
+
 /*consultarCEP.addEventListener('click', async function(){
  let url = "https://viacep.com.br/ws/18136280/json";
 
@@ -17,37 +19,34 @@ console.log(cep.logradouro)
 
 }); */
 
-
+//Para lidar com função que usa await, é necessário que ela seja async function
 consultarCEP.addEventListener('click', async function(){
     erro.classList.remove('mostrar');
     erro.classList.add('esconder');
-  let newCep = parseInt(inputCEP.value);
- let url = `https://viacep.com.br/ws/${newCep}/json`;
+    let newCep = parseInt(inputCEP.value);
+    //Insere os valores do campo na requisição da API
+    let url = `https://viacep.com.br/ws/${newCep}/json`;
 
+    //Tenta fazer a busca de dados da API e se retornar dados, insere na variavel
  let response = await fetch(url);
 
+//Verificar dados da variavel
  console.log(response);
-if (!response.ok){
-    
-    /*let cep = await response.json();
+//Após receber os dados, transfere eles para um objeto 
+let cep = await response.json();
 
-    inputLogradouro.value = cep.logradouro;
-    inputBairro.value = cep.bairro;
-    inputCidade.value = cep.localidade;*/
- 
-
- } else{
+//Se não acha o cep e retorna erro, ele mostra o erro, se não, preenche os campos vazios
+if (cep.erro == "true"){
     erro.classList.add('mostrar');
     erro.classList.remove('esconder');
     erro.innerHTML = "CPF invalido!";
- }
-
-    let cep = await response.json();
-
-    inputLogradouro.value = cep.logradouro;
+} else {
+     inputLogradouro.value = cep.logradouro;
     inputBairro.value = cep.bairro;
     inputCidade.value = cep.localidade;
-    
+ 
+}
+
 
 
 
